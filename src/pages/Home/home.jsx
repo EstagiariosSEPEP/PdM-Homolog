92c012cc1b76de628009a1f1d0442db6f114db22
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-05-29 14:04:42 -0300

    limpando a home page

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,28 +1,11 @@
-import Line from "../../components/line/line";
-import { useEffect } from "react";
-import { scrollToSection } from "../../utils/scroll";
+import AllSections from "./sections/all-sections";
+
 
 function Home() {
-  useEffect(() => {
-    const links = document.querySelectorAll('a[href^="#"]');
-
-    links.forEach((link) => {
-      link.addEventListener("click", (event) => {
-        event.preventDefault(); // Evita o salto direto
-        const id = link.getAttribute("href").substring(1);
-        scrollToSection(id);
-      });
-    });
-
-    return () => {
-      links.forEach((link) => {
-        link.removeEventListener("click", () => {});
-      });
-    };
-  }, []);
+  
 
   return (
-    <Line />
+    <AllSections/>
   );
 }
 
