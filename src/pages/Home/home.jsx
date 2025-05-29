ab4ffea490a49ecbc9f6072d162cbb864d11098d
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-05-29 11:05:13 -0300

    cleaning the components

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,10 +1,4 @@
 import Line from "../../components/line/line";
-import About from "./SectionAbout";
-import Description from "./description";
-import Banner from "../../components/banner/banner";
-import SectionEixos from "./SectionEixos";
-import Audiences from "../Audiences/Audiences";
-import ProgramasMetasAnteriores from "../programa-metas/ProgramasMetasAnteriores";
 import { useEffect } from "react";
 import { scrollToSection } from "../../utils/scroll";
 
@@ -28,38 +22,7 @@ function Home() {
   }, []);
 
   return (
-    <div>
-      <div className="banner">
-        <Banner />
-      </div>
-      <div className="container-description">
-        <Description />
-      </div>
-      <div className="flex flex-nowrap items-center justify-center">
-        <Line />
-      </div>
-      <div id="sobre" className="container-about">
-        <About />
-      </div>
-      <div className="flex flex-nowrap items-center justify-center">
-        <Line />
-      </div>
-      <div className="container-eixo">
-        <SectionEixos/>
-      </div>
-      <div className="flex flex-nowrap items-center justify-center">
-        <Line />
-      </div>
-      <div id="audiencias" className="container-audiences">
-        <Audiences />
-      </div>
-      <div className="flex flex-nowrap items-center justify-center">
-        <Line />
-      </div>
-      <div id="programas">
-        <ProgramasMetasAnteriores />
-      </div>
-    </div>
+    <Line />
   );
 }
 
