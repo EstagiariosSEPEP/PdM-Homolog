afc5a7cdaaaf35fe12526f0bb0e2bf50f856df78
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-05-29 14:04:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
+import Home from './pages/Home/home';
 import { HashRouter } from 'react-router-dom';
 import './App.css'
 
-import Footer from './components/Footer/footer';
-import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home/home';
 
 function App() {
   return (
     <HashRouter>
-        <div className='home-app'>
-            <div className='navbar'>
-                <Navbar />
-            </div>
-            <div className='content'>
-                <div className='home'>
-                    <Home />
-                </div>
-            </div>
-            <div className='footer'>
-                <Footer />
-            </div>
-        </div>
+       <Home/>
     </HashRouter>
   );
 }
