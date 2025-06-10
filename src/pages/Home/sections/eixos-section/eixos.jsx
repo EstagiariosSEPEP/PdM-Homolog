bae88012b5d922ac40c15ee3f44be84f7127f41d
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-06-10 15:10:16 -0300

    corrigindo posicionamento

--- src/pages/Home/sections/eixos-section/eixos.jsx
+++ src/pages/Home/sections/eixos-section/eixos.jsx
@@ -3,9 +3,10 @@ import EixosSection from '../../../../components/grid/grid_card_eixos'
 
 export default function Eixos() {
   return (
-    <div>
-        <div>
-            <h2 className='text-[color:var(--color-blue-950)] h-40 text-[5rem] pl-8 pr-4 py-4'>eixos temáticos</h2>
+    <div className='pt-24'>
+        <div className='w-[110rem] left-12 h-1 bg-[black] absolute'></div>
+        <div className='relative left-20 pt-8 w-[40rem]'>
+            <h2 className='text-[color:var(--color-blue-950)] h-40 pl-8 pr-4 py-4 h2-eixos w-[40rem]'>eixos temáticos</h2>
             <div className="animated_line"></div>
         </div>
       <div className='w-full'>
